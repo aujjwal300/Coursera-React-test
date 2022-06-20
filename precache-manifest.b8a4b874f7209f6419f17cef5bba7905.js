f11ee27626ea035d308f1fcd95edcc668e8ea20f
aujjwal300 <ujjwalagrawal300@gmail.com>
2022-06-20 21:06:17 +0530

    Updates

---
+++ precache-manifest.b8a4b874f7209f6419f17cef5bba7905.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d635b4adbb5b66309dc",
+    "url": "/Coursera-React-test/static/css/main.26f4268f.chunk.css"
+  },
+  {
+    "revision": "9d635b4adbb5b66309dc",
+    "url": "/Coursera-React-test/static/js/main.9d635b4a.chunk.js"
+  },
+  {
+    "revision": "17a3d24873f6441f0ccd",
+    "url": "/Coursera-React-test/static/js/1.17a3d248.chunk.js"
+  },
+  {
+    "revision": "1be1f4150d8c3e9c9223",
+    "url": "/Coursera-React-test/static/css/2.5248fdf6.chunk.css"
+  },
+  {
+    "revision": "1be1f4150d8c3e9c9223",
+    "url": "/Coursera-React-test/static/js/2.1be1f415.chunk.js"
+  },
+  {
+    "revision": "6806908341f309a465dd",
+    "url": "/Coursera-React-test/static/js/runtime~main.68069083.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Coursera-React-test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Coursera-React-test/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Coursera-React-test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Coursera-React-test/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Coursera-React-test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "d12532c40d6c48af29c5b80d2d327b8b",
+    "url": "/Coursera-React-test/index.html"
+  }
+];
